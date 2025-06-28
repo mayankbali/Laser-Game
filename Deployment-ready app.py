aebb00b34abd8897b1a5f6c10da4fda11ae3d7c4
Mayank Bali <mayankbali3@gmail.com>
2025-06-28 23:28:51 +0530

    Added code for Railway

--- Deployment-ready app.py
+++ Deployment-ready app.py
@@ -1349,3 +1349,8 @@ game_manager = GameManager()
     </script>
 </body>
 </html>
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port, debug=False)
